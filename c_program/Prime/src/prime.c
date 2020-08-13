a79fb637d6a284db0368ea0a9c079cf179c812bb
Kailash Ganpath <69031013+stepin105519@users.noreply.github.com>
2020-08-14 05:04:50 +0530

    Create prime.c

---
+++ c_program/Prime/src/prime.c
@@ -0,0 +1,19 @@
+#include "prime.h"
+
+int prime(int number)
+{
+  int iter;
+
+  /* Return -1 for neither prime nor composite numbers */
+  if(number == 0 || number == 1)
+    return -1;
+  /* Return 1 for prime number 0 for Composite number */
+  
+  for(iter = 2; iter <= (number/2); iter++)
+  {
+    if(number%iter == 0)
+      return 0;
+  }
+
+  return 1;
+}
