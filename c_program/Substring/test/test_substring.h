6c44f4f5c6422b23985dbf49280afe456e9bdc48
Kailash Ganpath <69031013+stepin105519@users.noreply.github.com>
2020-08-14 01:52:56 +0530

    Add test header

---
+++ c_program/Substring/test/test_substring.h
@@ -0,0 +1,16 @@
+/**
+* @file test_substring.h
+*
+*/
+
+#ifndef __TEST_SUBSTRING_H__
+#define __TEST_SUBSTRING_H__
+
+/**
+* It is for unity based testing c file scripted to test the substring.c function 
+* It compares actual and expected output and result Pass/Failed Testcases based on preconfigured Test case functions
+*/
+
+int test_substring_main(void);
+
+#endif //ifndef __TEST_SUBSTRING_H__
