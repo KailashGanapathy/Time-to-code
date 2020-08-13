b74ad9368e6a9cc39b7486c45a46b46705008fbe
Kailash Ganpath <69031013+stepin105519@users.noreply.github.com>
2020-08-14 05:03:39 +0530

    Add files via upload

---
+++ c_program/Prime/inc/prime.h
@@ -0,0 +1,18 @@
+/**
+* @file prime.h
+*
+*/
+#ifndef __PRIME_H__
+#define __PRIME_H__
+
+/**
+* Check whether the integer number prime
+* @param number for which  prime number check has to be found
+* @return whether the number is a prime number
+* @note Returns 1 for prime number
+* @note Returns 0 for composite number
+* @note Returns -1 for 1/0 for neither prime nor composite numbers 
+*/
+int prime(int number);
+
+#endif /* #ifndef __PRIME_H__ */
