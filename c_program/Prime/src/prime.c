9b385868c9a6fc1ffd7177eb66c248a205596058
Kailash Ganpath <69031013+stepin105519@users.noreply.github.com>
2020-08-14 06:13:48 +0530

    Update prime.c

--- c_program/Prime/src/prime.c
+++ c_program/Prime/src/prime.c
@@ -1,4 +1,14 @@
+/// @file prime.c
+
 #include "prime.h"
+
+/**
+* This function used to check whether the input number is a Prime number
+* @param[in] number the input number need to check the condition
+* @note Return 1 (Number is a prime number)
+* @note Return 0 (Number is a composite number)
+* @note Return -1 (Number is a negative or neither prime nor composite)
+*/
 
 int prime(int number)
 {
@@ -8,6 +18,12 @@ int prime(int number)
   if(number <= 1)
     return -1;
   /* Return 1 for prime number 0 for Composite number */
+  
+  /**
+  * A Number will have atleat 2 factors if it divisible by any natural number ranges [1 - Number]
+  * The loop start from 2 to half of the number
+  * if it is a composite it immediately return 0
+  */
   
   for(iter = 2; iter <= (number/2); iter++)
   {
